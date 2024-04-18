461e8ecf87803dbc329e475c976ed674a913c625
mmahendran7 <mmahendran7@gatech.edu>
2024-04-18 17:56:19 -0400

    fix error

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const baselineGenPath = baseImagePath + "generated_images/";
 
 // Function to generate image cell HTML
 function generateImageCell(imageId, prompt, isStable) {
-    const imagePath = (isStable ? stableGenPath : baselineGenPath) + imageId.replace('.jpg', '') + "_" + prompt.replace(/ /g, '_') + ".jpg";
+    const imagePath = (isStable ? stableGenPath : baselineGenPath) + imageId + "_" + prompt.replace(/ /g, '_') + ".jpg";
     const scores = scoresDictionary[imageId][prompt];
     console.log(imagePath)
     console.log(scoresDictionary)
