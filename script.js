4dab54e071a538f37a10b929f5017b2a334067b2
mmahendran7 <mmahendran7@gatech.edu>
2024-04-18 18:01:40 -0400

    fix error

--- script.js
+++ script.js
@@ -191,6 +191,7 @@ function generateImageCell(imageId, prompt, isStable) {
     console.log(imageId)
     console.log(imagePath)
     console.log(scoresDictionary)
+    console.log(scores)
     const scoreText = `FID: ${scores.FID.toFixed(2)}<br>PSNR: ${scores.PSNR.toFixed(2)}<br>SSIM: ${scores.SSIM.toFixed(2)}`;
     return `<div class="image-cell">
       <img src="${imagePath}" alt="${prompt}" />
