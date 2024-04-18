38336e809edcf40aba3ff25c588f29ed26d59510
mmahendran7 <mmahendran7@gatech.edu>
2024-04-18 17:28:09 -0400

    try javascript code to display images

---
+++ script.js
@@ -0,0 +1,220 @@
+// This should be your actual scores dictionary
+const scoresDictionary = {
+
+        "000000000139.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 4.813334941864014,
+                "A lion with Sunglasses": 2.7885825634002686,
+                "Horse running": 4.1096625328063965
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 10.099922401953616,
+                "A lion with Sunglasses": 9.629691503348772,
+                "Horse running": 12.061393784032612
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.3136525659604354,
+                "A lion with Sunglasses": 0.18902334085390163,
+                "Horse running": 0.39944666951687974
+            }
+        },
+        "000000000285.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 31.0989933013916,
+                "A lion with Sunglasses": 6.9699320793151855,
+                "Horse running": 30.90654945373535
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 8.347786612079938,
+                "A lion with Sunglasses": 7.751030715175129,
+                "Horse running": 10.025925592636613
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.1102370010072513,
+                "A lion with Sunglasses": 0.08289389164326123,
+                "Horse running": 0.1337812221957896
+            }
+        },
+        "000000000632.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 23.93022918701172,
+                "A lion with Sunglasses": 8.596115112304688,
+                "Horse running": 20.78570556640625
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 10.415009896605753,
+                "A lion with Sunglasses": 9.190363099603932,
+                "Horse running": 13.01417992113739
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.16744900472463262,
+                "A lion with Sunglasses": 0.06122938557673396,
+                "Horse running": 0.22113808422884165
+            }
+        },
+        "000000000724.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 27.038745880126953,
+                "A lion with Sunglasses": 6.354520797729492,
+                "Horse running": 12.712190628051758
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 9.241865650732993,
+                "A lion with Sunglasses": 8.667211267604195,
+                "Horse running": 10.27140548283819
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.29685802669235684,
+                "A lion with Sunglasses": 0.1770073310157648,
+                "Horse running": 0.25534608233549655
+            }
+        },
+        "000000000776.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 34.05232620239258,
+                "A lion with Sunglasses": 9.228991508483887,
+                "Horse running": 28.53932762145996
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 9.315271437955507,
+                "A lion with Sunglasses": 9.194010489220407,
+                "Horse running": 11.190233760376675
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.11442932528409488,
+                "A lion with Sunglasses": 0.07594917093396601,
+                "Horse running": 0.11781874080995552
+            }
+        },
+        "000000000785.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 4.907434463500977,
+                "A lion with Sunglasses": 7.314289093017578,
+                "Horse running": 2.39872407913208
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 9.261570561579608,
+                "A lion with Sunglasses": 9.268078480635518,
+                "Horse running": 9.732272921293436
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.49580223712405674,
+                "A lion with Sunglasses": 0.34306814586790185,
+                "Horse running": 0.49954430194487637
+            }
+        },
+        "000000000802.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 1.8235214948654175,
+                "A lion with Sunglasses": 13.219362258911133,
+                "Horse running": 2.4640305042266846
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 9.836696386368047,
+                "A lion with Sunglasses": 8.851808975808543,
+                "Horse running": 10.851693551819679
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.30158295794089685,
+                "A lion with Sunglasses": 0.21798429360496066,
+                "Horse running": 0.3668808549683824
+            }
+        },
+        "000000000872.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 17.961700439453125,
+                "A lion with Sunglasses": 4.602916717529297,
+                "Horse running": 28.32120704650879
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 10.93102295662824,
+                "A lion with Sunglasses": 8.535973837416723,
+                "Horse running": 11.701717556514224
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.2074016885008573,
+                "A lion with Sunglasses": 0.084834155568091,
+                "Horse running": 0.20847111981686986
+            }
+        },
+        "000000000885.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 2.19897198677063,
+                "A lion with Sunglasses": 4.833423137664795,
+                "Horse running": 0.9457703828811646
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 12.590673184401439,
+                "A lion with Sunglasses": 11.406755139008675,
+                "Horse running": 14.707719836258146
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.4520628979696726,
+                "A lion with Sunglasses": 0.3290426888994694,
+                "Horse running": 0.490954503412806
+            }
+        },
+        "000000001000.jpg": {
+            "FID": {
+                "A dog with Sunglasses": 46.62409210205078,
+                "A lion with Sunglasses": 10.776712417602539,
+                "Horse running": 26.82111358642578
+            },
+            "PSNR": {
+                "A dog with Sunglasses": 8.125714892995799,
+                "A lion with Sunglasses": 8.589480879846509,
+                "Horse running": 9.490072753141538
+            },
+            "SSIM": {
+                "A dog with Sunglasses": 0.14600425155265198,
+                "A lion with Sunglasses": 0.10289934342683359,
+                "Horse running": 0.14538894455234108
+            }
+        }
+    
+  };
+
+const baseImageIds = ["000000000139.jpg","000000000285.jpg","000000000632.jpg","000000000724.jpg",
+"000000000776.jpg","000000000785.jpg","000000000802.jpg","000000000872.jpg","000000000885.jpg","000000001000.jpg"]
+
+const prompts = ["A dog with Sunglasses", "A lion with Sunglasses", "Horse running"];
+
+// Base directory paths
+const baseImagePath = "data/coco_dataset/val2017/";
+const stableGenPath = baseImagePath + "generated_images_stable/";
+const baselineGenPath = baseImagePath + "generated_images/";
+
+// Function to generate image cell HTML
+function generateImageCell(imageId, prompt, isStable) {
+    const imagePath = (isStable ? stableGenPath : baselineGenPath) + imageId.replace('.jpg', '') + "_" + prompt.replace(/ /g, '_') + ".jpg";
+    const scores = scoresDictionary[imageId][prompt];
+    const scoreText = `FID: ${scores.FID.toFixed(2)}<br>PSNR: ${scores.PSNR.toFixed(2)}<br>SSIM: ${scores.SSIM.toFixed(2)}`;
+    return `<div class="image-cell">
+      <img src="${imagePath}" alt="${prompt}" />
+      <div class="scores">${scoreText}</div>
+    </div>`;
+}
+
+// Function to build the table
+function buildTable() {
+  let tableHTML = '<div class="image-table">';
+
+  baseImageIds.forEach(imageId => {
+    tableHTML += '<div class="image-column">';
+    tableHTML += `<div class="prompt-title">Base Image</div>`;
+    tableHTML += `<img src="${baseImagePath + imageId}.jpg" alt="Base Image">`;
+    prompts.forEach(prompt => {
+      // Stable model images
+      tableHTML += generateImageCell(imageId, prompt, true);
+      // Baseline model images
+      tableHTML += generateImageCell(imageId, prompt, false);
+    });
+    tableHTML += '</div>';
+  });
+
+  tableHTML += '</div>';
+  document.getElementById('imageTable').innerHTML = tableHTML;
+}
+
+// Call buildTable on page load
+window.onload = buildTable;
